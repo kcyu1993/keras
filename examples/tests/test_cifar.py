ac1e173a4eba093d0f3f27e5bc6a99f6e4a4913c
kcyu2014 <kaicheng.yu@epfl.ch>
2016-12-16 15:56:13 +0100

    Keras daily build

--- examples/tests/test_cifar.py
+++ examples/tests/test_cifar.py
@@ -1,8 +1,10 @@
 import pytest
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 
 from keras.applications.resnet50 import ResCovNet50CIFAR
 from keras.utils.test_utils import image_classification, keras_test
-from kyu.models.cifar import cifar_fitnet_v2, cifar_fitnet_v1, cifar_fitnet_v3
+from kyu.models.cifar import cifar_fitnet_v2, cifar_fitnet_v1, cifar_fitnet_v3, model_snd
 
 nb_class = 10
 input_shape = (3, 32, 32)
@@ -35,21 +37,28 @@ input_shape = (3, 32, 32)
 #     image_classification(model, nb_class=10, input_shape=input_shape, fit=False)
 
 
+# @keras_test
+# def test_fitnet_v3_params():
+#     params = [[], [50], [100], [100, 50], [16, 8], [32, 16], [16, 32]]
+#     nb_epoch = 200
+#     cov_outputs = [500, 100, 50, 10]
+#     for cov_output in cov_outputs:
+#         for param in params:
+#             print("Run routine 13 nb epoch {} param mode {}".format(nb_epoch, param))
+#             for mode in range(0,10):
+#                 model = cifar_fitnet_v3(parametrics=param, epsilon=0, mode=mode,
+#                                         nb_classes=nb_class, dropout=False,
+#                                         cov_mode='o2transform', cov_branch_output=cov_output)
+#                 image_classification(model, nb_class=nb_class, input_shape=input_shape, fit=False)
+
+
 @keras_test
-def test_fitnet_v3_params():
-    params = [[], [50], [100], [100, 50], [16, 8], [32, 16], [16, 32]]
-    nb_epoch = 200
-    cov_outputs = [500, 100, 50, 10]
-    for cov_output in cov_outputs:
-        for param in params:
-            print("Run routine 13 nb epoch {} param mode {}".format(nb_epoch, param))
-            for mode in range(0,10):
-                model = cifar_fitnet_v3(parametrics=param, epsilon=0, mode=mode,
-                                        nb_classes=nb_class, dropout=False,
-                                        cov_mode='o2transform', cov_branch_output=cov_output)
-                image_classification(model, nb_class=nb_class, input_shape=input_shape, fit=False)
+def test_log_model():
+    model = model_snd(parametrics=[])
+    image_classification(model, nb_class=nb_class, input_shape=input_shape, fit=True)
 
 
 if __name__ == '__main__':
+
     pytest.main([__file__])
 
